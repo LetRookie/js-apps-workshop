3d9d17a75ea5523199f214c5ee2e8329ba57d0f8
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-10-12 12:32:55 +0300

    new changes

--- lesson-03/base/src/register.js
+++ lesson-03/base/src/register.js
@@ -5,42 +5,40 @@ async function onSubmit(event) {
 
     const formData = new FormData(event.target);
 
-
     const email = formData.get('email');
     const password = formData.get('password');
     const repass = formData.get('rePass');
 
-
     try {
         if (email == '' || password == '') {
             throw new Error('All fields must be filled!');
-        };
-
-        if (password != repass) {
-            throw new Error('Passwords don\'t match!');
         }
 
-        const response = await fetch('http://localhost:3030/users/register', {
+        if (password != repass) {
+            throw new Error('Passwords don\'t match');
+        }
+
+        let response = await fetch('http://localhost:3030/users/register', {
             method: 'post',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'apllication/json'
             },
-            body: JSON.stringify({
+            body : JSON.stringify({
                 email,
                 password
             })
         });
 
-        if (response.ok == false) {
+        if(response.ok == false) {
             const error = await response.json();
             throw Error(error.message);
         }
 
-        data = await response.json();
+        const data = await response.json();
         sessionStorage.setItem('accessToken', data.accessToken);
-        window.location = '/index.html';
+        window.location = '/';
 
     } catch (err) {
-        alert(err.message)
+        alert(err.message);
     }
 }
