5199414effc28b3fd7d90d3278ebc05a515f288a
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-10-12 08:58:09 +0300

    new update

---
+++ lesson-03/base/src/register.js
@@ -0,0 +1,46 @@
+document.querySelector('form').addEventListener('submit', onSubmit);
+
+async function onSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const repass = formData.get('rePass');
+
+
+    try {
+        if (email == '' || password == '') {
+            throw new Error('All fields must be filled!');
+        };
+
+        if (password != repass) {
+            throw new Error('Passwords don\'t match!');
+        }
+
+        const response = await fetch('http://localhost:3030/users/register', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        if (response.ok == false) {
+            const error = await response.json();
+            throw Error(error.message);
+        }
+
+        data = await response.json();
+        sessionStorage.setItem('accessToken', data.accessToken);
+        window.location = '/index.html';
+
+    } catch (err) {
+        alert(err.message)
+    }
+}
